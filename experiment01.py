0f895334c765f01082511811301502ef84a2b0e8
John Pertell <pertelj@sunypoly.edu>
2024-11-20 16:16:34 -0500

    Added better file handling

--- experiment01.py
+++ experiment01.py
@@ -17,22 +17,39 @@ def compute_vfi_video(video_path, output_name, model_pth="model/FLAVR_2x.pth", i
     if os.path.exists(model_pth) == False: raise Exception("ERROR: Can not find model from path.")
 
     orig_cnt, orig_fps, orig_time = get_frame_data(video_path)
+    fps = int(orig_fps)
 
     # if not in slow_motion => use a higher res video instead by multiplying fps by interpolation factor
     if slow_motion == True:
-        fps = int(orig_fps)
         os.system(f"python interpolate.py --input_video {video_path} --factor {interpolation} --load_model {model_pth} --output_fps {fps}")
     else:
-        fps = int(orig_fps * interpolation)
+        fps *= int(interpolation)
         os.system(f"python interpolate.py --input_video {video_path} --factor {interpolation} --load_model {model_pth} --output_fps {fps}")
 
-    # rename interpolated video to the output video + .avi extension
-    new_path = video_path[:-4] + f"_{interpolation}x.avi"
-    os.rename(new_path, output_name+".avi")
+    try:
+        # rename interpolated video to the output video + .avi extension
+        new_path = video_path[:-4] + f"_{interpolation}x.avi"
+        output_name = output_name+".avi"
+        if os.path.exists(output_name) == True: 
+            remove_flag = input(f"File: {output_name} already exists. Remove? ")
+            if remove_flag.lower()[0] == "y":
+                os.remove(output_name)
+            else:
+                output_name = output_name[:-4]
+                output_name += "(NEW).avi"
+        os.rename(new_path, output_name)
+    except Exception as e:
+        print(e)
+        output_name = video_path
+    
+    print(f"VFI VIDEO SAVED AS: {output_name}")
+    return output_name, fps
 
 
 def main():
-    compute_vfi_video("noice.mp4", "new_noice", "model/FLAVR_2x.pth", interpolation=2, slow_motion=False)
+    interp = 8
+    vfi_info = compute_vfi_video("noice.mp4", "new_noice", f"model/FLAVR_{interp}x.pth", interpolation=interp, slow_motion=True)
+    print(vfi_info)
 
 
 if __name__ == "__main__":
